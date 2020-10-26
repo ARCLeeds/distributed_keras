8456adc0d4e9198210563faac7d8676a6b69e198
Alex Coleman <alexjcoleman@gmail.com>
2020-10-26 16:18:22 +0000

    adjusting this script

--- rapids/multi_node_RF.py
+++ rapids/multi_node_RF.py
@@ -22,81 +22,85 @@ import dask_cudf
 from cuml.dask.ensemble import RandomForestClassifier as cumlDaskRF
 from sklearn.ensemble import RandomForestClassifier as sklRF
 
-## using dask to setup cluster
+def main():
+    ## using dask to setup cluster
 
-# This will use all GPUs on the local host by default
-cluster = LocalCUDACluster(threads_per_worker=1)
-c = Client(cluster)
+    # This will use all GPUs on the local host by default
+    cluster = LocalCUDACluster(threads_per_worker=1)
+    c = Client(cluster)
 
-# Query the client for all connected workers
-workers = c.has_what().keys()
-n_workers = len(workers)
-n_streams = 8 # Performance optimization
+    # Query the client for all connected workers
+    workers = c.has_what().keys()
+    n_workers = len(workers)
+    n_streams = 8 # Performance optimization
 
-## setting parameters
+    ## setting parameters
 
-# Data parameters
-train_size = 100000
-test_size = 1000
-n_samples = train_size + test_size
-n_features = 20
+    # Data parameters
+    train_size = 100000
+    test_size = 1000
+    n_samples = train_size + test_size
+    n_features = 20
 
-# Random Forest building parameters
-max_depth = 12
-n_bins = 16
-n_trees = 1000
+    # Random Forest building parameters
+    max_depth = 12
+    n_bins = 16
+    n_trees = 1000
 
-## generating some data
-# use sklearn API to generate some psuedo data to test this out
-# we do this on the client (initial process) before distributing this out to workers
+    ## generating some data
+    # use sklearn API to generate some psuedo data to test this out
+    # we do this on the client (initial process) before distributing this out to workers
 
-X, y = datasets.make_classification(n_samples=n_samples, n_features=n_features,
-                                 n_clusters_per_class=1, n_informative=int(n_features / 3),
-                                 random_state=123, n_classes=5)
-y = y.astype(np.int32)
-X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=test_size)
+    X, y = datasets.make_classification(n_samples=n_samples, n_features=n_features,
+                                    n_clusters_per_class=1, n_informative=int(n_features / 3),
+                                    random_state=123, n_classes=5)
+    y = y.astype(np.int32)
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=test_size)
 
-n_partitions = n_workers
+    n_partitions = n_workers
 
-# First convert to cudf (with real data, you would likely load in cuDF format to start)
-X_train_cudf = cudf.DataFrame.from_pandas(pd.DataFrame(X_train))
-y_train_cudf = cudf.Series(y_train)
+    # First convert to cudf (with real data, you would likely load in cuDF format to start)
+    X_train_cudf = cudf.DataFrame.from_pandas(pd.DataFrame(X_train))
+    y_train_cudf = cudf.Series(y_train)
 
-# Partition with Dask
-# In this case, each worker will train on 1/n_partitions fraction of the data
-X_train_dask = dask_cudf.from_cudf(X_train_cudf, npartitions=n_partitions)
-y_train_dask = dask_cudf.from_cudf(y_train_cudf, npartitions=n_partitions)
+    # Partition with Dask
+    # In this case, each worker will train on 1/n_partitions fraction of the data
+    X_train_dask = dask_cudf.from_cudf(X_train_cudf, npartitions=n_partitions)
+    y_train_dask = dask_cudf.from_cudf(y_train_cudf, npartitions=n_partitions)
 
-# Persist to cache the data in active memory
-X_train_dask, y_train_dask = \
-  dask_utils.persist_across_workers(c, [X_train_dask, y_train_dask], workers=workers)
+    # Persist to cache the data in active memory
+    X_train_dask, y_train_dask = \
+      dask_utils.persist_across_workers(c, [X_train_dask, y_train_dask], workers=workers)
 
-# do this model fit with cpus using sklearn
-# Use all avilable CPU cores
-t0 = time.time()
-skl_model = sklRF(max_depth=max_depth, n_estimators=n_trees, n_jobs=-1)
-skl_model.fit(X_train, y_train)
-t1 = time.time()
+    # do this model fit with cpus using sklearn
+    # Use all avilable CPU cores
+    t0 = time.time()
+    skl_model = sklRF(max_depth=max_depth, n_estimators=n_trees, n_jobs=-1)
+    skl_model.fit(X_train, y_train)
+    t1 = time.time()
 
-cpu_time = t1 - t0
+    cpu_time = t1 - t0
 
-print(f"CPU model fit took: {cpu_time}.")
+    print(f"CPU model fit took: {cpu_time}.")
 
-## fit model using Dask cuML
-t0 = time.time()
-cuml_model = cumlDaskRF(max_depth=max_depth, n_estimators=n_trees, n_bins=n_bins, n_streams=n_streams)
-cuml_model.fit(X_train_dask, y_train_dask)
+    ## fit model using Dask cuML
+    t0 = time.time()
+    cuml_model = cumlDaskRF(max_depth=max_depth, n_estimators=n_trees, n_bins=n_bins, n_streams=n_streams)
+    cuml_model.fit(X_train_dask, y_train_dask)
 
-wait(cuml_model.rfs) # Allow asynchronous training tasks to finish
-t1 = time.time()
-gpu_time = t1 - t0 
+    wait(cuml_model.rfs) # Allow asynchronous training tasks to finish
+    t1 = time.time()
+    gpu_time = t1 - t0 
 
-print(f"cuML model fit time: {gpu_time}.")
+    print(f"cuML model fit time: {gpu_time}.")
 
-## compare accuracy
-skl_y_pred = skl_model.predict(X_test)
-cuml_y_pred = cuml_model.predict(X_test)
+    ## compare accuracy
+    skl_y_pred = skl_model.predict(X_test)
+    cuml_y_pred = cuml_model.predict(X_test)
 
-# Due to randomness in the algorithm, you may see slight variation in accuracies
-print("SKLearn accuracy:  ", accuracy_score(y_test, skl_y_pred))
-print("CuML accuracy:     ", accuracy_score(y_test, cuml_y_pred))
+    # Due to randomness in the algorithm, you may see slight variation in accuracies
+    print("SKLearn accuracy:  ", accuracy_score(y_test, skl_y_pred))
+    print("CuML accuracy:     ", accuracy_score(y_test, cuml_y_pred))
+
+if __name__ == "__main__":
+    main()
